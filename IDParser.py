ccd12e6ff243a1511c6cebb1d51e36f600681100
Mwandia <JEMwandia@gmail.com>
2019-09-10 12:46:42 +0300

    first commit

---
+++ IDParser.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+'''
+This parses a picture of a Kenyan ID card, into the text and 
+uses the text to verify the ID. 
+'''
+
+# Identifiers (can't think of the right word)
+DOB = "DATE OF BIRTH"
+SEX = "SEX"
+DIST_O_B = "DISTRICT OF BIRTH"
+SERIAL_NUM = "SERIAL NUMBER"
+NAMES = "FULL NAMES"
+DOI = "DATE OF ISSUE"
+POI = "PLACE OF ISSUE"
+ID = "ID NUMBER"
+DIST = "DISTRICT"
+DIV = "DIVISION"
+LOC = "LOCATION"
+SUB_LOC = "SUB-LOCATION"
+
+
+
+
+def constant_list():
+    pass
+
+def extract_text():
+    pass
+
+def extract_image():
+    pass
+
+def extract_information():
+    pass
